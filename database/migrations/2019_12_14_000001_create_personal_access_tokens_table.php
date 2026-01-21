d055316feccd5e8026af1d8a21a73621a5166a5f
Kleber Alves <kleberjuniorr63@gmail.com>
2026-01-21 14:16:27 -0300

    feat(migrations-unicas): consolidar migrations e soft deletes base

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
@@ -20,12 +17,15 @@ return new class extends Migration
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
+            $table->softDeletes();
+            
+            $table->foreign('tokenable_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('personal_access_tokens');
